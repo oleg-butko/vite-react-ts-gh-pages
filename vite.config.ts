cade0109fe65f3ac6d8b1f82651f1639edf9337e
Oleg Butko <oleg.butko@gmail.com>
2025-04-04 15:32:35 +0300

    fix tabler icons causing slow startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    base: '/vite-react-ts-gh-pages/',
-    server: {
-      fs: {
-        cachedChecks: false
-      }
-    }
-});
-
-
+	plugins: [react(), tsconfigPaths()],
+	base: '/vite-react-ts-gh-pages/',
+	resolve: {
+		alias: {
+			// https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+			'@tabler/icons-react':
+				'@tabler/icons-react/dist/esm/icons/index.mjs'
+		}
+	}
+})
